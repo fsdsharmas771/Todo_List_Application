5bd5e08b739053d8b1160a080a2e36311cdbbe19
Tarun Sharma <fsdsharmas771@gmail.com>
2023-01-09 12:19:19 +0530

    Create routes controller and render view with the help of view ening ejs

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use('/',require('./routes/index'));
+app.set('view engin','ejs');
+app.set('views','./views');
+app.use(express.static('./assets'));// without this we cant use static files
 
 app.listen(port,function(error){
     if(error){
